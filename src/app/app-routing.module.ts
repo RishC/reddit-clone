c15a3aef322cfe8627be2c452f75b8b954d58144
RishC <rishc94@gmail.com>
2018-01-20 23:00:33 +0530

    fix syntax

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: './auth/auth.module#AuthModule', }
+  { path: 'login', loadChildren: './auth/auth.module#AuthModule' }
 ];
 
 @NgModule({
