c0f2b854dbca81880667d5ad6901d2564b668ab1
RishC <rishc94@gmail.com>
2018-01-20 23:20:34 +0530

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: './auth/auth.module#AuthModule' }
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
