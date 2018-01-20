680dacc15c7444edfbb9c1aa9b447088acfe514d
RishC <rishc94@gmail.com>
2018-01-20 16:42:50 +0530

    scaffolding auth module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthComponent } from './auth.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: AuthComponent },
+  { path: 'register', component: AuthComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ],
+  declarations: [AuthComponent],
+  exports: [RouterModule]
+})
+export class AuthModule { }
